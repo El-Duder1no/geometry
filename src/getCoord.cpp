e1aecac4d30b192cc601ad16ed74aaffaf89e4ef
Andrey <udovchenkoae@gmail.com>
2020-04-21 01:36:29 +0700

    Create void GetCoord

---
+++ src/getCoord.cpp
@@ -0,0 +1,8 @@
+#include "circle.h"
+#include "getCoord.h"
+#include <iostream>
+
+void GetCoord(Circle one)
+{
+    std::cout << "circle(" << one.x << " " << one.y << ", " << one.r << ")" << std::endl;
+}
